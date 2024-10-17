832105b1249303d44fc1282fbe161c64ab2a5ba8
Fabrizio9898 <fabrizioandrade989@gmail.com>
2024-10-17 19:00:09 -0300

    exception filter

--- src/dtos/menu/create-menu.dto.ts
+++ src/dtos/menu/create-menu.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer"
-import { ArrayMinSize, IsArray, IsNotEmpty, IsString, IsUUID, isUUID, Length, ValidateNested } from "class-validator"
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString, Length, ValidateNested } from "class-validator"
 import { CreateDishDto } from "../dish/create-dish.dto"
 
 export class CreateMenuDto {
@@ -7,9 +7,6 @@ export class CreateMenuDto {
     @Length(3, 40)
     @IsString()
     name: string
-    @IsNotEmpty()
-    @IsUUID()
-    establishment: string
     @IsArray()
     @ArrayMinSize(1, { message: 'Cada menú debe tener al menos un platillo' })
     @ValidateNested({ each: true, message: ' Cada platillo debe ser válido' })
