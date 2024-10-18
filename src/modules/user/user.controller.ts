f6117bb0ece15faa6d20c4e7450b9e5588309be2
JoshuaFrancoG <elvisteck046@gmail.com>
2024-10-18 11:01:45 -0500

    ADDED/ validationPipe, updatePassword Dto y metodos

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,8 @@
 import { BadRequestException, Body, Controller, Delete, Get, Param, ParseUUIDPipe, Put } from "@nestjs/common";
 import { UpdateUserDto } from "src/dtos/user/updateUser.dto";
 import { UserService } from "./user.service";
-import { isNotEmptyObject } from "class-validator";
-import { ApiTags } from "@nestjs/swagger";
+import { isNotEmpty, isNotEmptyObject } from "class-validator";
+import { ApiBody, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { SanitizedUserDto } from "src/dtos/user/sanitizedUser.dto";
 
 @ApiTags("User")
@@ -11,21 +11,37 @@ export class UserController {
 
     constructor(private readonly userService: UserService) { }
 
+
     @Get(":id")
+    @ApiOperation({ summary: "obtiene un usuario por su id" })
     async getUser(@Param("id", ParseUUIDPipe) id: string): Promise<SanitizedUserDto> {
         return await this.userService.getUserById(id);
     }
 
     @Put(":id")
+    @ApiOperation({ summary: "actualiza la informacion de un usuario, por id y body" })
+    @ApiBody({
+        schema: {
+            example: {
+                name: 'Tim Haward',
+                country: "Noruega"
+            }
+        }
+    })
     async updateUser(@Param("id", ParseUUIDPipe) id: string, @Body() modified_user: UpdateUserDto): Promise<SanitizedUserDto> {
         if (!isNotEmptyObject(modified_user)) {
             throw new BadRequestException("body values are empty");
+        }
+
+        if (isNotEmpty(modified_user.password)) {
+            throw new BadRequestException("You can't modify passwords in this endpoint!");
         }
 
         return await this.userService.updateUser(id, modified_user);
     }
 
     @Delete(":id")
+    @ApiOperation({ summary: "elimina un usuario por su id (posiblemente no se vaya a usar)" })
     async deleteUser(@Param("id", ParseUUIDPipe) id: string): Promise<any> {
         return await this.userService.deleteUser(id);
     }
