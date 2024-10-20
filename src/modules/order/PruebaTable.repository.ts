301d7c151ebaff3654538284e054fdc371b269d4
Omar <omaraguilalira2@gmail.com>
2024-10-19 19:05:48 -0600

    Adición de status a Order y creación de OrderDetail

---
+++ src/modules/order/PruebaTable.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Restaurant_Table } from "src/entities/tables.entity";
+import { Repository, Table } from "typeorm";
+
+// Sólo para probar mientras está listo en repositorio oficial de Table
+
+@Injectable()
+export class TableRepository{
+    constructor(@InjectRepository(Restaurant_Table) private tableRepository: Repository<Restaurant_Table>){}
+    async getTableById(id: string): Promise<null | Restaurant_Table> {
+        const foundedTable: null | Restaurant_Table = await this.tableRepository.findOneBy({id})
+        return foundedTable;
+    }
+}
---
+++ src/modules/order/order_detail/orderDetail.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { OrderedDishesDto } from "src/dtos/order/ordered_dishes.dto";
+import { Dish } from "src/entities/dish.entity";
+import { DishService } from "src/modules/dish/dish.service";
+import { OrderDetailRepository } from "./orderDetail.repository";
+import { OrderDetail } from "src/entities/orderDetail.entity";
+
+@Injectable()
+export class OrderDetailService {
+    constructor(private readonly dishService: DishService, private readonly orderDetailRepository: OrderDetailRepository){}
+    async createOrderDetail(dishes: OrderedDishesDto[]): Promise<OrderDetail> {
+        const dishesEntities: Dish[] = await this.dishService.getDishesByIds(dishes);
+        const price: number = dishes.reduce((acumulador, dishPedido) => {
+            const dishEntity = dishesEntities.find(dishEntity => dishEntity.id === dishPedido.id);
+            acumulador + (dishEntity.price * dishPedido.quantity);
+            return acumulador;
+          }, 0);
+        return this.orderDetailRepository.createOrderDetail(dishesEntities, price);
+    }
+}
