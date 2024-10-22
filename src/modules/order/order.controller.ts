ede5b8b9446d245d4bdabceac59b3218f1a413b4
Omar <omaraguilalira2@gmail.com>
2024-10-22 10:06:18 -0600

    adición de documentacion para orders

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiConsumes, ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
 import { OrderService } from "./order.service";
 import { CreateOrderDto } from "src/dtos/order/create-order.dto";
 import { UpdateOrderDto } from "src/dtos/order/update-order.dto";
@@ -16,13 +16,15 @@ export class OrderController {
     }
 
     @Put(":id")
-    @ApiOperation({ summary: "Devuelve una orden en específico, si es que existe, utilizando su id" })
+    @ApiOperation({ summary: "Modifica una orden en especifico usando su id y los datos que se desean cambiar" })
+    @ApiParam({ name: 'id', type: String, description: 'ID de una orden' })
     async updateOrder(@Param("id", ParseUUIDPipe) id: string, @Body() OrderToModify: UpdateOrderDto) {
         return await this.orderService.updateOrder(id, OrderToModify);
     }
 
     @Delete(":id")
-    @ApiOperation({ summary: "Elimina el registro de una orden es especifico usando su id" })
+    @ApiOperation({ summary: "Elimina el registro de una orden en especifico usando su id" })
+    @ApiParam({ name: 'id', type: String, description: 'ID de una orden' })
     async deleteOrder(@Param("id", ParseUUIDPipe) id: string){
         return await this.orderService.deleteOrder(id);
     }
