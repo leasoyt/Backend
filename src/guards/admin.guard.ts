3a88aec6e123d37135c92949819e697540e4eeaa
Fabrizio9898 <fabrizioandrade989@gmail.com>
2024-11-01 17:48:44 -0300

    chat module

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,41 +1,32 @@
-import { CanActivate, ExecutionContext, Injectable, ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { config as dotenvConfig } from 'dotenv';
-import { log } from 'console';
-import { UserRole } from 'src/enums/roles.enum';
 
 dotenvConfig({ path: '.env' });
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-
-  constructor(private jwtService: JwtService) { }
+  constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    console.log('tokee',token);
-    
-
+    console.log(token);
     if (!token) {
       throw new UnauthorizedException('Token missing');
     }
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET
+        secret: process.env.JWT_SECRET,
       });
-
-      payload.exp = new Date(payload.exp * 1000);
-      payload.int = new Date(payload.int * 1000);
-      payload.role =
-        payload.role === UserRole.MANAGER
-          ? [UserRole.MANAGER]
-          : payload.role === UserRole.WAITER
-            ? [UserRole.WAITER]
-            : [UserRole.CONSUMER];
-console.log('payload',payload);
 
       // Verificar si el usuario es administrador usando el campo isAdmin
       if (!payload.isAdmin) {
