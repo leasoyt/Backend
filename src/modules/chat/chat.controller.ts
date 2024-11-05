0444d7cd9f15e74c14f23250a84e1ea76ae26609
Omar <omaraguilalira2@gmail.com>
2024-11-05 10:26:03 -0600

    documentacion swagger

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,9 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  @ApiOperation({
+    summary: 'Permite iniciar una conversación con el chatbot'
+  })
   @Post()
   async createResponse(@Body() data: { message: string ,userId:string}) {
     const response = await this.chatService.processingMessage(data.message,data.userId);
--- src/modules/mail/mail.controller.ts
+++ src/modules/mail/mail.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Email')
 @Controller('mail')
 export class MailController {
     constructor(
         private readonly mailService: MailService
     ){}
+
+    @ApiOperation({
+        description: 'Endpoint de prueba para enviar notificaciones por email sobre actuailzaciones del estado de la suscripción de un usuario',
+        summary: 'Endpoint de prueba para enviar notificación por emal a un usuario'
+    })
     @Post()
     async probarEnvioSuscripciones(){
-        
         return this.mailService.sendNotificationEmail()
     }
 }
